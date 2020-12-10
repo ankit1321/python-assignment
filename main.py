e0809d0cfeb016b2d3fc7cd7c4964558700adba3
Ankit Prasad <36136033+ankit1321@users.noreply.github.com>
2020-12-10 18:10:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import argparse
 import os
 from imutils import paths
 import numpy as np
-
+import pygame.camera
 
 class Bipolar:
     
@@ -22,11 +22,14 @@ class Bipolar:
         self.known_face_names= []
         self.camera = cv2.VideoCapture(0)
         pygame.init()
+        pygame.camera.init()
         pygame.font.init()
         pygame.display.set_caption(machine_name)
+        self.clist = pygame.camera.list_cameras()
+        print(self.clist)
         self.screen = pygame.display.set_mode([640, 480])
         self.font_state = pygame.font.SysFont("TIMES NEW ROMAN", 40)
-        self.font = pygame.font.SysFont("Arial", 25)
+        self.font = pygame.font.SysFont("Arial", 20)
         self.white = (255,255,255)
         self.black = (0,0,0)
         self.green = (0,255,0)
@@ -59,13 +62,14 @@ class Bipolar:
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             frame1 = frame.swapaxes(0, 1)
             pygame.surfarray.blit_array(self.screen, frame1)
-            pygame.display.update()
+            
             face_locations = face_recognition.face_locations(frame)
             if len(face_locations)>0:
                 if self.state == 'MachineInactive':
                     self.identified()
                 unknown_face_encodings = face_recognition.face_encodings(frame,face_locations)
                 machine_state_status = self.font_state.render('Machine in Active State',True,self.white,self.black)
+                print(self.state)
                 self.screen.blit(machine_state_status,(130,10))    	
                 face_names = []
                 for face_encoding in unknown_face_encodings:
@@ -81,15 +85,15 @@ class Bipolar:
                     for (top,right,bottom,left),name in zip(face_locations,face_names):
                         if name=="unknown":
                             pygame.draw.rect(self.screen, (255,0,0), pygame.Rect(left, top, right-left, bottom-top),  2) 
-                            render_name = self.font.render(name, True,self.red,self.black)
-                            self.screen.blit(render_name,(left,top-50))
+                            render_name = self.font.render(name.upper(), True,self.red,self.black)
+                            self.screen.blit(render_name,(left,top-35))
                             print("unknown")
                             pygame.display.update()
                         else:
                         # for (top,right,bottom,left) in face_locations:
                             pygame.draw.rect(self.screen, (0,255,0), pygame.Rect(left, top, right-left, bottom-top),  2) 
                             render_name = self.font.render(name.upper(), True, self.green,self.black)
-                            self.screen.blit(render_name,(left,top-50))
+                            self.screen.blit(render_name,(left,top-35))
                             print(name)
                             pygame.display.update()
 
@@ -98,9 +102,9 @@ class Bipolar:
                     self.not_identified()
                     machine_state_status = self.font_state.render('Machine in Inactive State',True,self.white,self.black)
                     self.screen.blit(machine_state_status,(130,10))
-                    print("Application is inactive")
-            pygame.display.update()
-            time.sleep(1)
+                    pygame.display.update()
+                    print(self.state)
+            # time.sleep(.5)
 
 
 
@@ -128,4 +132,5 @@ if __name__=="__main__":
     bipolar.read_known_images(imagePaths)
     bipolar.video_streaming()
     
+        
     
